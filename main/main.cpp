ca97fbd159a8046e78de3351c51393e38bcf9224
Justin T Conroy <justintconroy@users.noreply.github.com>
2021-01-26 01:26:03 -0600

    Feature/testable (#1)
    * Add unit test infrastructure.
    * Add a component to test.

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,17 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+
+#include "IdfTest.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+    void app_main();
+}
+#endif
 
 void app_main(void)
 {
@@ -34,6 +35,11 @@ void app_main(void)
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+
+    IdfTest::MyTestClass t{};
+    auto result = t.TestableMethod(1, 1);
+    printf("IDF Test Result: %d\n", result);
+
     printf("Restarting now.\n");
     fflush(stdout);
     esp_restart();
